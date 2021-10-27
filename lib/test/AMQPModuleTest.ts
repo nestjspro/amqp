f4f87427d9eb2987644bec03baae95c75e8c272c
Matthew Davis <matthew@matthewdavis.io>
2021-10-27 10:56:39 -0400

    bump

--- lib/test/AMQPModuleTest.ts
+++ lib/test/AMQPModuleTest.ts
@@ -48,13 +48,13 @@ describe('AMQPModule Test', () => {
         const service = module.get(AMQPService);
         const publisher = module.get(AMQPPublisher);
 
-        service.connections[ 0 ].reference$.subscribe(reference => {
-            console.log(3);
-            console.log(reference);
-
-        });
-        console.log(4);
-        console.log(service.connections);
+        // service.connections[ 0 ].reference$.subscribe(reference => {
+        //     console.log(3);
+        //     console.log(reference);
+        //
+        // });
+        // console.log(4);
+        // console.log(service.connections);
         // await publisher.connect();
         //
         // const channel = await publisher.amqp.createChannel();
